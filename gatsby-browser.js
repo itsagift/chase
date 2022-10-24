7260f4fcdbb9d9bf92f8e1946c856db338a31a23
itsagift <30632225+itsagift@users.noreply.github.com>
2022-10-23 22:55:51 -0400

    Addressed font loading problem

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
 import "./src/style.css"
+import "@fontsource/open-sans"
--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,20 @@ module.exports = {
           families: ['Archivo', 'Tenor Sans']
         }
       }
-    }
+    },
+    // {
+    //   resolve: `gatsby-omni-font-loader`,
+    //   options: {
+    //     enableListener: true,
+    //     preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+    //     web: [
+    //       {
+    //         name: `Open Sans`,
+    //         file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+    //       },
+    //     ],
+    //   },
+    // }
     
   ],
 }
