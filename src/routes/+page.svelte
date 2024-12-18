d5f540ef9c71a968aea37724b1b1309b02df8395
itsagift <30632225+itsagift@users.noreply.github.com>
2024-12-18 01:04:34 -0500

    Add forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,20 @@
 <center>
 <h1>Chase Reynolds</h1>
 <p>Website very much in progress.</p>
+<p>In the meantime, please contact me to discuss tutoring/classes:</p>
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message for me!<br> </label>
+    <textarea name="message"></textarea>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 </center>
