06c4f6f310b345f97b638f392e453124a9ce0194
itsagift <30632225+itsagift@users.noreply.github.com>
2024-12-18 01:10:01 -0500

    changed site title

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<!-- src/routes/__layout.svelte -->
+<script context="module" lang="ts">
+  export const prerender = true;
+</script>
+
+<script>
+  import { onMount } from "svelte";
+  
+  onMount(() => {
+    document.title = "Chase Reynolds"; 
+  });
+</script>
+
+<slot />
